33605984c38d5a1b0b232dd9f167f363b1d9d7ec
luisa.colorado <luisa.colorado.e@gmail.com>
2025-06-20 15:36:06 -0500

    upload last changes to review CI pipeline

--- src/main/java/pom/pages/TodoListPage.java
+++ src/main/java/pom/pages/TodoListPage.java
@@ -4,10 +4,15 @@ import net.serenitybdd.core.pages.PageObject;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class TodoListPage extends PageObject {
 
     @FindBy(className = "new-todo")
     private WebElement addItem;
+
+    @FindBy(id="list")
+    public List<WebElement> listOfSettings;
 
     public void addTask(String item) {
         addItem.sendKeys(item);
--- src/test/java/runners/cucumber/stepdefinitions/TodoStepDefinitions.java
+++ src/test/java/runners/cucumber/stepdefinitions/TodoStepDefinitions.java
@@ -1,29 +1,38 @@
 package runners.cucumber.stepdefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
+import io.cucumber.java.DataTableType;
 import io.cucumber.java.ParameterType;
+import io.cucumber.java.Transpose;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.serenitybdd.screenplay.*;
+import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
-import net.serenitybdd.screenplay.conditions.ConditionalPerformable;
-import net.serenitybdd.screenplay.conditions.ConditionalPerformableOnQuestion;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Convert;
+import org.openqa.selenium.WebElement;
+import pom.pages.TodoListPage;
+import screenplay.model.Label;
 import screenplay.questions.todolist.Items;
-import screenplay.questions.travelocity.DepartureFlight;
 import screenplay.tasks.todolist.AddAnItem;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 
 public class TodoStepDefinitions {
+
+    TodoListPage todoListPage;
 
     @Before
     public void setStage () {
@@ -41,7 +50,7 @@ public class TodoStepDefinitions {
         );
     }
 
-    @When("he adds {string} to his list")
+    @When("he adds {} to his list")
     public void he_adds_to_his_list(String toDo) {
         theActorInTheSpotlight().attemptsTo(
                 AddAnItem.called(toDo));
@@ -53,5 +62,62 @@ public class TodoStepDefinitions {
                 seeThat(Items.toDo(), containsInAnyOrder(expectedItems.toArray()))
         );
     }
+
+    @Given("parameters are collected as map")
+    public void parametersAreCollectedAsMap(List<Map<String, String>> data) {
+     System.out.println("data processed is " + data);
+    }
+
+    @DataTableType()
+    public Map<String, String> convert(Map<String, String> parametersMap){
+        return parametersMap.entrySet()
+                .stream()
+                .map(TodoStepDefinitions::removeQuotesFromEntryValue)
+                .collect(HashMap::new,
+                        (newMap, originalMap) -> newMap.put(originalMap.getKey(), nullIfEmpty(originalMap.getValue())),
+                        HashMap::putAll
+                );
+    }
+
+    public static Map.Entry<String, String> removeQuotesFromEntryValue(Map.Entry<String, String> entry) {
+        return Map.entry(entry.getKey(), entry.getValue().replaceAll("\"", ""));
+    }
+
+    public static Map.Entry<String, String> replaceNullIfEmpty(Map.Entry<String, String> entry) {
+        return Map.entry(entry.getKey(), entry.getValue().replaceAll("", null));
+    }
+
+    /**
+     * Returns null if the input is empty.
+     *
+     * @param param The input string.
+     * @return The param or null if the input is empty.
+     */
+    public static String nullIfEmpty(String param){
+        return !param.isEmpty() ? param : null;
+    }
+
+    @Given("parameters are collected as list")
+    public void parametersAreCollectedAsList(List<String> data) {
+        System.out.println(data);
+    }
+
+    @When("parameters are transposed in a list")
+    public void parametersAreTransposedAsList(@Transpose List<String> entry) {
+        System.out.println(entry);
+    }
+
+    @Then("the setting is available")
+    public void stepThree(DataTable settingsTable){
+            List<String> options = settingsTable.asList(String.class);
+            List<String> matchingOptions = settingsTable.asList(String.class);
+        SoftAssertions sa = new SoftAssertions();
+        sa.assertThat(matchingOptions)
+                .as("All options are available")
+                .containsExactlyInAnyOrderElementsOf(options);
+            sa.assertAll();
+        }
+
+
 
 }
